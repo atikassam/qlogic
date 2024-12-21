c1b517b3b81baf66e11fb1f2281c3652b1c9e096
Atikur Rahman <atikassam@outlook.com>
2024-12-22 01:29:45 +0530

    fix(github-action): yaml format issue is fixed

--- packages/react-mui/src/lib/react-mui.spec.tsx
+++ packages/react-mui/src/lib/react-mui.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import ReactMui from './react-mui';
 
 describe('ReactMui', () => {
-  it('should render successfully. ok', () => {
+  it('should render successfully. ok1', () => {
     const { baseElement } = render(<ReactMui />);
     expect(baseElement).toBeTruthy();
   });
