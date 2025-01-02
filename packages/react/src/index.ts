bb53d947d2e0db9b3af7b16db1f96f772cf55494
Atikur Rahman <atikassam@outlook.com>
2025-01-02 21:23:08 +0530

    feat(qlogic): execution ctx added

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,4 +2,5 @@ export * from './lib/QLogicBuilder/QLogicBuilder';
 export * from './lib/QLogicBuilder/QLogicBuilderProvider';
 export * from './lib/QLogicEnvironment';
 export * from './lib/QLogicBuilder/QLogicCodePreview';
-export { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
+export { LightAsync as SyntaxHighlighter, type SyntaxHighlighterProps } from 'react-syntax-highlighter';
+export * as highlighterStyles from 'react-syntax-highlighter/dist/esm/styles/hljs';
