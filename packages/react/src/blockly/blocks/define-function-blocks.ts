7fff2064476904885efe25075d043579c0dac775
Atikur Rahman <atikassam@outlook.com>
2025-01-05 03:43:01 +0530

    fix(qlogic): overwrites previous definition. issue fixed

--- packages/react/src/blockly/blocks/define-function-blocks.ts
+++ packages/react/src/blockly/blocks/define-function-blocks.ts
@@ -4,6 +4,8 @@ import { QLogicEnvironmentFunc } from '../../lib/QLogicEnvironment';
 import DefineFunc from './define-func';
 
 export function defineFunctionBlock(func: QLogicEnvironmentFunc) {
+  if (javascript.javascriptGenerator.forBlock[DefineFunc.name(func)]) return;
+
   Blockly.common.defineBlocks({
     [DefineFunc.name(func)]: DefineFunc.Block(func),
   });
